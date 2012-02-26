8e3640fc61c0466a0d2a6bd671d0f71fe9e4ad5f
Flaviu Toader <flaviu.toader@gmail.com>
2012-02-26 17:50:48 +0200

    Added a map property editor using treeview.

---
+++ src/client/widgets/mappropertyeditor.cpp
@@ -0,0 +1,86 @@
+
+#include <iostream>
+#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include <Wt/WContainerWidget>
+#include <Wt/WStandardItem>
+#include <Wt/WStandardItemModel>
+#include <Wt/WTreeView>
+#include <Wt/WText>
+#include <Wt/WString>
+#include <Wt/WPanel>
+#include <Wt/WFlags>
+#include <Wt/WModelIndex>
+#include <Wt/WApplication>
+#include <Wt/WEnvironment>
+
+#include "mappropertyeditor.h"
+#include "logger.h"
+
+using namespace Wt;
+
+MapPropertyEditor::MapPropertyEditor(WStandardItemModel *model) : model(model)
+{
+    new WText(tr("mappropertyeditor.title"), this);
+
+    // create the view.
+    WPanel *panel = new WPanel(this);
+    //panel->resize(WLength::Auto, WLength::Auto);
+    panel->resize(600, 400);
+    panel->setCentralWidget(treeView = new WTreeView());
+
+    if(!WApplication::instance()->environment().ajax()) 
+    {
+        treeView->resize(WLength::Auto, 380);
+    }
+
+    treeView->setAlternatingRowColors(true);
+    treeView->setRowHeight(25);
+    treeView->setModel(model);
+    treeView->setColumnWidth(0, WLength(285));
+    treeView->setColumnAlignment(0, AlignCenter);
+    treeView->setColumnWidth(1, WLength(285));
+    treeView->setColumnAlignment(1, AlignCenter);
+
+    treeView->setExpanded(model->index(0, 0), true);
+    treeView->setExpanded(model->index(0, 0, model->index(0, 0)), true);
+}
+
+WStandardItemModel* MapPropertyEditor::createModel(WObject *parent) 
+{
+    WStandardItemModel *result = new WStandardItemModel(0, 2, parent);
+
+    result->setHeaderData(0, Horizontal, tr("mappropertyeditor.header.propertyname").toUTF8());
+    result->setHeaderData(1, Horizontal, tr("mappropertyeditor.header.propertyvalue").toUTF8());
+
+    result->appendRow(groupItem(tr("mappropertyeditor.group.general").toUTF8()));
+    result->appendRow(propertyItem(tr("mappropertyeditor.group.general.dimx").toUTF8(), 0, EditRole));
+    result->appendRow(propertyItem(tr("mappropertyeditor.group.general.dimy").toUTF8(), 0, EditRole));
+
+    return result;
+}
+
+WStandardItem *MapPropertyEditor::groupItem(const std::string& groupName)
+{
+    WStandardItem *result = new WStandardItem(groupName);
+    result->setColumnCount(2);
+    return result;
+}
+
+std::vector<WStandardItem *> MapPropertyEditor::propertyItem(const std::string& name, const boost::any& data, int role)
+{
+    std::vector<WStandardItem *> result;
+    WStandardItem *item;
+
+    item = new WStandardItem(name);
+    result.push_back(item);
+
+    item = new WStandardItem;
+    item->setData(data, role);
+    item->setFlags(WFlags<ItemFlag>(ItemIsEditable));
+    result.push_back(item);
+
+    return result;
+}
+
---
+++ src/client/widgets/mappropertyeditor.h
@@ -0,0 +1,71 @@
+/*
+ *    <one line to give the program's name and a brief idea of what it does.>
+ *    Copyright (C) 2012  Flaviu Toader <email>
+ * 
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ * 
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ * 
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MAPPROPERTYEDITOR_H_
+#define MAPPROPERTYEDITOR_H_
+
+#include <Wt/WContainerWidget>
+#include <Wt/WFlags>
+#include <Wt/WModelIndex>
+#include <boost/any.hpp>
+
+// forward declarations
+namespace Wt {
+    class WStandardItem;
+    class WStandardItemModel;
+    class WTreeView;
+    class WString;
+}
+
+//! A treeview allowing to set/change properties for VANET maps.
+/*!
+ * This class extends the Wt::WContainerWidget, and creates a static mvc tree containing the editable properties of
+ * a VANET map. 
+ */
+class MapPropertyEditor : public Wt::WContainerWidget
+{
+public:
+    //! Constructor takes the model to be used.
+    /*!
+     * This property editor constructor takes in a Wt::StandardItemModel as the model to be rendered using the treeview.
+     */
+    MapPropertyEditor(Wt::WStandardItemModel *model);
+
+    //! Get the treeview.
+    /*! 
+     * Returns the treeview used in this property editor.
+     */
+    Wt::WTreeView *getTreeView() const { return treeView; }
+
+    //! Static method that generates the model for our map property editor.
+    /*!
+     * A static method that creates the model for the treeview. This model will contain all the necessary properties
+     * for a VANET map.
+     */
+    static Wt::WStandardItemModel *createModel(WObject *parent);
+
+private:
+    static Wt::WStandardItem *groupItem(const std::string& groupName);
+    static std::vector<Wt::WStandardItem *> propertyItem(const std::string& name, const boost::any& data, int role);
+
+    Wt::WStandardItemModel *model;
+    Wt::WTreeView *treeView;
+
+};
+
+#endif // MAPPROPERTYEDITOR_H_
